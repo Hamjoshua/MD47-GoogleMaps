6aaee9334f9a08339e6c6e8ebbb47edb9d9aeb8e
Hamjoshua <biryukov-10@list.ru>
2025-05-10 18:05:29 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MD-47-GoogleMaps"
+include(":app")
