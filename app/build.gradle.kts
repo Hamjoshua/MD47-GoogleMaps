d0623cbed46530355738c0bab6e8b0bb07c6f549
Hamjoshua <biryukov-10@list.ru>
2025-05-11 12:29:23 +0700

    Добавлен минимум для Google карт

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
     buildFeatures {
         buildConfig = true
+        viewBinding = true
     }
 }
 
@@ -48,6 +49,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // geo
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
 }
 
 
--- app/src/main/java/com/example/md_47_googlemaps/MainActivity.kt
+++ app/src/main/java/com/example/md_47_googlemaps/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.md_47_googlemaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.md_47_googlemaps.databinding.ActivityMainBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var googleMap : GoogleMap
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment : SupportMapFragment = supportFragmentManager
+            .findFragmentById(R.id.googleMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
     }
 }
