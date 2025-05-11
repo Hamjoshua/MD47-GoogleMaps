9fd59f30dc104f8d39b56eeb4d0dd50dfcb74f89
Hamjoshua <biryukov-10@list.ru>
2025-05-11 15:42:10 +0700

    1 задание выполнено
    - Зум на местоположение
    - Зум на библиотеку

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.md_47_googlemaps"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.md_47_googlemaps"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -46,9 +46,14 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation("androidx.activity:activity:1.10.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.core:core-ktx:1.15.0")
 
     // geo
     implementation("com.google.android.gms:play-services-maps:19.2.0")
--- app/src/main/java/com/example/md_47_googlemaps/MainActivity.kt
+++ app/src/main/java/com/example/md_47_googlemaps/MainActivity.kt
@@ -1,26 +1,126 @@
 package com.example.md_47_googlemaps
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.md_47_googlemaps.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var googleMap : GoogleMap
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationManager: LocationManager
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Разрешение получено
+
+            } else {
+
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initGoogleObjects()
+        initUI()
+    }
+
+    fun initGoogleObjects(){
         val mapFragment : SupportMapFragment = supportFragmentManager
             .findFragmentById(R.id.googleMap) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
+    fun initUI(){
+        binding.postOfficeBtn.setOnClickListener{
+            moveToPostOffice()
+        }
+
+        binding.myLocButton.setOnClickListener{
+            checkPermission(Manifest.permission.ACCESS_FINE_LOCATION,
+                ::toMyLocation)
+        }
     }
 
     override fun onMapReady(p0: GoogleMap) {
+        googleMap = p0
+    }
+
+    fun checkPermission(permission: String, void: () -> Unit){
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                void()
+
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this, permission
+            ) -> {
+                // Показываем объяснение перед запросом
+                requestPermissionLauncher.launch(permission)
+            }
+
+            else -> {
+                // Просто запрашиваем разрешение
+                requestPermissionLauncher.launch(permission)
+            }
+        }
+    }
+
+    fun moveToPostOffice(){
+        val postOfficeCoords = LatLng(55.354993, 86.085805)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(postOfficeCoords, 17f))
+    }
+
+    fun toMyLocation(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Log.d("MyLocation", "Doing stuff")
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val myLocation = LatLng(it.latitude, it.longitude)
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 17f))
+                }
+            }
+        }
+        else {
+            Toast.makeText(this, "Включите GPS", Toast.LENGTH_LONG).show()
+        }
 
     }
 }
